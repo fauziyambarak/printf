b261a367f04f77868665860e3334a7a617618447
fauziyambarak <30287125+fauziyambarak@users.noreply.github.com>
2023-09-18 14:43:04 +0300

    Update handle_print.c
    Updated

--- handle_print.c
+++ handle_print.c
@@ -10,6 +10,7 @@
  * @precision: Precision specification
  * @size: Size specifier
  * Return: 1 or 2;
+ * handle file
  */
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
