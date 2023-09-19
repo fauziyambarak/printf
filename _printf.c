4a441f1930a6868a6f50e313133580acf6329a7f
fauziyambarak <30287125+fauziyambarak@users.noreply.github.com>
2023-09-19 13:11:41 +0400

    Updated now

--- _printf.c
+++ _printf.c
@@ -6,7 +6,7 @@ void print_buffer(char buffer[], int *buff_ind);
  * _printf - Printf function
  * @format: format.
  * Return: Printed chars.
- * C files
+ * C file
  */
 int _printf(const char *format, ...)
 {
